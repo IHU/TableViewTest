347b58664275619e7b9ab1231f1d9a92bbc29ec1
Filiz Uslu <filizuslu@Filizs-MacBook-Pro.local>
2013-02-22 00:52:05 +0100

    Initial Commit

---
+++ TableViewTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TableViewTest
+//
+//  Created by Filiz Uslu on 22/02/13.
+//  Copyright (c) 2013 Filiz Uslu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TableViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewTest
+//
+//  Created by Filiz Uslu on 22/02/13.
+//  Copyright (c) 2013 Filiz Uslu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
